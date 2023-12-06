33baac2b4fb9b77e61965d1f1be249f3d4883b5d
Vitor Reis <vitorsouza.reis@estudante.ufjf.br>
2023-12-06 19:55:55 -0300

    adicionando os arquivos

---
+++ estoque.js
@@ -0,0 +1,16 @@
+//estoque.js
+
+let estoque = {
+ joao: [
+        {tipo: "maca", qtd: 1},
+        {tipo: "pera", qtd: 1},
+],
+ maria: [
+        {tipo:"maca", qtd: 2},
+        {tipo:"banana", qtd: 4},
+],
+};
+
+estoque.joao[0].tipo
+
+
