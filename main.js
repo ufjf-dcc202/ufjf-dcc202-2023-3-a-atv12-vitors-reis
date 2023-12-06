869d1f35cf8374be14b7f67c4cec6830f75a5425
Vitor Reis <vitorsouza.reis@estudante.ufjf.br>
2023-12-06 20:58:02 -0300

    atualizações

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 
 document.entrada.addEventListener('submit', leFormulario);
+
+const olJoao = document.querySelector('#olJoao');
+const olMaria = document.querySelector("#olMaria")
 function leFormulario(event) {
 
     event.preventDefault();
+                    
     const fruta = document.entrada.fruta.value;
     const quantidade = document.entrada.quantidade.value;
 
     console.log('fruta: ${fruta} qtd: ${quantidade}');
+    event.submit();
 }
