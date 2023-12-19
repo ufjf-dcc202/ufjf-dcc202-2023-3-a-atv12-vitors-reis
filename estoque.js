07df5628a61032647ebf146b574a9ae8ed27e7ce
Vitor Reis <vitorsouza.reis@estudante.ufjf.br>
2023-12-19 19:54:54 -0300

    coreções

--- estoque.js
+++ estoque.js
@@ -1,19 +1,73 @@
-//estoque.js
-
 let estoque = {
- joao: [
-        {tipo: "maca", 'qtd' : 1},
-        {tipo: "pera", 'qtd' : 1},
-    ],
- maria: [
-        {'tipo':"maca", 'qtd' : 2},
-        {'tipo':"banana", 'qtd' : 4},
-    ],
+    'joao': [{'tipo': 'maca', 'quantidade': 1}],
+    'maria': [{'tipo': 'maca', 'quantidade': 2}]
 };
 
-export function getEstoque(){
-    return structuredClone(estoque);
+function getEstoque() {
+    return structuredClone(estoque)
 }
 
-//export {getEstoque}
+function limpaEstoque() {
+    estoque = {}
+}
 
+function transacaoNoEstoque(origem, destino, tipo, quantidade) {
+    if (origem === destino || quantidade <= 0) {
+        return
+    }
+  
+    if (origem !== "pomar" && !estoque[origem]) {
+        estoque[origem] = []
+    }  
+  
+    if (destino !== "pomar" && !estoque[destino]) {
+        estoque[destino] = []
+    }  
+    
+    if (destino === "pomar") {
+        const itemEncontrado = estoque[origem].find(item => item.tipo === tipo)
+    
+        if (itemEncontrado) {
+            itemEncontrado.quantidade = Math.max(0, itemEncontrado.quantidade - quantidade)
+        }
+    
+        return
+    }  
+
+    if (origem === "pomar") {
+        const itemEncontrado = estoque[destino].find(item => item.tipo === tipo)
+    
+        if (itemEncontrado) {
+            itemEncontrado.quantidade += quantidade
+        } else {
+            estoque[destino].push({tipo, quantidade})
+        }
+        
+        return
+    } else {
+        const itemOrigem = estoque[origem].find(item => item.tipo === tipo)
+        const itemDestino = estoque[destino].find(item => item.tipo === tipo)
+
+        if (!itemOrigem) {
+            return
+        } else if (quantidade > itemOrigem.quantidade) {
+            if (itemDestino) {
+                itemDestino.quantidade += itemOrigem.quantidade
+            } else {
+                estoque[destino].push({tipo: tipo, quantidade: itemOrigem.quantidade})
+            }
+            itemOrigem.quantidade = 0
+        } else {
+            if (itemDestino) {
+                itemDestino.quantidade += quantidade
+            } else {
+                estoque[destino].push({tipo, quantidade})
+            }
+            itemOrigem.quantidade -= quantidade
+        }
+    }
+    
+    return
+}
+
+export {getEstoque, limpaEstoque, transacaoNoEstoque}
--- main.js
+++ main.js
@@ -1,15 +1,52 @@
+import { getEstoque, limpaEstoque, transacaoNoEstoque } from "./estoque.js"
 
-document.entrada.addEventListener('submit', leFormulario);
+document.entrada.addEventListener('submit', leFormulario)
 
-const olJoao = document.querySelector('#olJoao');
-const olMaria = document.querySelector("#olMaria")
+const olJoao = document.querySelector("#joao")
+const olMaria = document.querySelector("#maria")
+
+atualizaTela()
+
 function leFormulario(event) {
+    event.preventDefault()
+    
+    const fruta = document.entrada.fruta.value
+    const quantidade = document.entrada.quantidade.valueAsNumber
+    const origem = document.entrada.origem.value
+    const destino = document.entrada.destino.value
+    
+    console.log(`solicitado: ${origem} doa ${quantidade} ${fruta} para ${destino}`)
 
-    event.preventDefault();
-                    
-    const fruta = document.entrada.fruta.value;
-    const quantidade = document.entrada.quantidade.value;
+    transacaoNoEstoque(origem, destino, fruta, quantidade)
+    atualizaTela()
+}
 
-    console.log('fruta: ${fruta} qtd: ${quantidade}');
-    event.submit();
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('botaoLimparLista').addEventListener('click', () => {
+        limpaEstoque()
+        atualizaTela()
+    })
+})
+
+function atualizaTela() {
+    const estoque = getEstoque()
+    
+    document.entrada.fruta.value = "maca"
+    document.entrada.quantidade.value = 1
+   
+    preencheListaPessoa(estoque['joao'], olJoao)
+    preencheListaPessoa(estoque['maria'], olMaria)
+}
+
+function preencheListaPessoa(pessoa, lista) {
+    lista.innerHTML = ""
+
+    if(Array.isArray(pessoa)) {
+        for (let i = 0; i < pessoa.length; i++) {
+            const monte = pessoa[i]
+            const eLi = document.createElement('li')
+            eLi.innerText = `${monte.tipo}: ${monte.quantidade}`
+            lista.append(eLi)
+        }
+    }
 }
